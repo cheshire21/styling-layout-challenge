efd94170baa9753700b140a54672ec664e5d9615
cheshire21 <coren114@gmail.com>
2022-10-06 10:17:41 -0500

    feat: add Menu tsx and css

---
+++ src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Menu.css";
+import Rectangle from "./Rectangle";
+
+function Menu() {
+  return (
+    <div className="div1">
+      <Rectangle name="rect1"/>
+    </div>
+  );
+}
+
+export default Menu;
