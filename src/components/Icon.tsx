fac7c85d751bcad4747aa9863fe78dd1a4ab5cfb
cheshire21 <coren114@gmail.com>
2022-10-06 15:13:21 -0500

    feat: modify Icon tsx and css

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import "../styles/Icon.css";
-import Img1 from '../images/img1.png'
 interface IProps {
     name:string
-    
+    image:any
 
 }
 
-function Icon({name} : IProps) {
+function Icon({image, name} : IProps) {
   return (
     
-        <img src={Img1} className={name}></img>
+        <img src={image} className={name}></img>
     
   );
 }
