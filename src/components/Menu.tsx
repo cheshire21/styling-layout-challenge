fe77b0ccd2c3bdc011b58214af8c13f69143cd01
cheshire21 <coren114@gmail.com>
2022-10-06 12:12:54 -0500

    feat: modify Menu tsx and css

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "../styles/Menu.css";
+import Icon from "./Icon";
+import ItemNavBar from "./ItemNavBar";
 import Rectangle from "./Rectangle";
 
 function Menu() {
   return (
     <div className="div1">
       <Rectangle name="rect1"/>
+      <ul className="navBar">
+        
+        <ItemNavBar name="Home" active />
+        
+        
+        <ItemNavBar name="My Files" />
+        
+        
+        <ItemNavBar name="Recent Files" />
+        
+        
+        <ItemNavBar name="Shared Filed " />
+        
+        
+        <ItemNavBar name="File Request" />
+        
+        
+        <ItemNavBar name="Trash" />
+        
+      </ul>
     </div>
   );
 }
