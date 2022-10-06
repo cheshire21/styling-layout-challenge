6557330502c5351d42323d59a2003d93b3019989
cheshire21 <coren114@gmail.com>
2022-10-06 15:12:30 -0500

    feat: modify ItemNavBar tsx and css

--- src/components/ItemNavBar.tsx
+++ src/components/ItemNavBar.tsx
@@ -6,15 +6,17 @@ import "../styles/ItemNavBar.css";
 interface IProps {
     name:string
     active?:boolean
+    type: string
+    image:any
 
 }
 
-function ItemNavBar({active, name} : IProps) {
+function ItemNavBar({image,type, active, name} : IProps) {
   return (
         <li className={active? "active" : "desactive"}>
-          <div className="itemNavBar_div">
-            <Icon name="small-icon" />
-            <label className="itemNavBar_text">{name}</label>
+          <div className={type === "nav"? "itemNavBar_nav_div" : "itemNavBar_list_div"}>
+            <Icon name="small-icon" image={image} />
+            <label className={type === "nav" ? "itemNavBar_text" : "itemNavBar_list_text"}>{name}</label>
           </div>
         </li>
   );
