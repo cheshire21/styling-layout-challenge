cbc7220f072ae27d8f70eea7c5a529bc3980fd5f
cheshire21 <coren114@gmail.com>
2022-10-07 15:13:43 -0500

    feat: modify TableItem tsx and css

---
+++ src/components/TableItem.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "../styles/TableItem.css";
+import Icon from "./Icon";
+import Rectangle from "./Rectangle";
+import Menu from '../images/menu.png'
+
+interface IProps{
+  member:number
+  name:string
+  date:string
+}
+
+function TableItem({member, name, date}:IProps) {
+  return (
+      
+        <tr>
+          <td>
+            <div className="name-cell">
+              <Rectangle name="rect4" />
+              <p className="name-cell-text">{name}</p>
+            </div>
+          </td>
+          <td>{member} members</td>
+          <td>
+            <div className="date-cell">
+              {date}
+              <Icon  image={Menu} name="table-icon"/>
+            </div>  
+          </td>
+        </tr>
+      
+  );
+} 
+
+export default TableItem;
