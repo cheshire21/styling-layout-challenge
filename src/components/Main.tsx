69c169ea161ed21bf713d401cef62ea9645554f7
cheshire21 <coren114@gmail.com>
2022-10-07 15:19:32 -0500

    feat: update main tsx and css

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,6 +4,7 @@ import Icon from "./Icon";
 import Img from '../images/magnifier.png'
 import Title from "./Title";
 import ListItem from "./ListItem";
+import TableItem from "./TableItem";
 
 function Main() {
   return (
@@ -24,12 +25,27 @@ function Main() {
       <Title text="Recently Files"/>
 
       <table className="table-files">
-      <tr className="table-files_header">
-        <th>Name</th>
-        <th>Members</th>
-        <th>Last Modified</th>
-      </tr>
+        <tr className="table-files_header">
+          <th>Name</th>
+          <th>Members</th>
+          <th>Last Modified</th>
+          
+        </tr>
+          <TableItem name="Travel Landing Page" member={5} date="Mar 8, 2020" />
+          <TableItem name="True Photos" member={12} date="Mar 8, 2020" />
+          <TableItem name="Dashboard Structure" member={10} date="Mar 9, 2020" />
+          <TableItem name="Character Illustration" member={3} date="Mar 10, 2020" />
       </table>
+
+      <Title text="Share with me"/>
+
+      <div className="div-list">
+        <ListItem titleProject="App Project" date="20.02.2020"  share/>
+        <ListItem titleProject="Project fitbit" date="28.02.2020" share/>
+        <ListItem titleProject="Client Documents" date="4.03.2020" share/>
+      </div>      
+
+
     </div>
   );
 } 
