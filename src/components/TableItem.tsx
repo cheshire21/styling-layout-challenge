90706602f3114916b04190483a88a0fb7bff63bf
cheshire21 <coren114@gmail.com>
2022-10-07 20:56:50 -0500

    feat: put horizontal menu

--- src/components/TableItem.tsx
+++ src/components/TableItem.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/TableItem.css";
 import Icon from "./Icon";
 import Rectangle from "./Rectangle";
-import Menu from '../images/menu.png'
+import Menu from '../images/menu-h.png'
 
 interface IProps{
   member:number
